ffc11d0b04df8f02efbc5dd268874d575bfe2c84
Urjit Vichhiya <vichhiyaurjit152@gmail.com>
2025-10-03 19:31:06 +0800

    Finalize DinD Jenkins setup with shared jenkins-data volume and security init script

--- jenkins_init/01-security.groovy
+++ jenkins_init/01-security.groovy
@@ -1,35 +1,27 @@
 #!groovy
-// Jenkins init script to enforce security using existing users
-// Users (urjit21838321, pipeline-user) must already exist in Jenkins
-
 import jenkins.model.*
 import hudson.security.*
 
 def instance = Jenkins.getInstance()
 
-// Use the existing security realm (do not create new users)
-def hudsonRealm = instance.getSecurityRealm()
+// Create local user database
+def hudsonRealm = new HudsonPrivateSecurityRealm(false)
+hudsonRealm.createAccount("admin", "AdminPass123!")   // admin user
+hudsonRealm.createAccount("pipelineuser", "PipelinePass123!") // limited user
 instance.setSecurityRealm(hudsonRealm)
 
-// Configure Matrix Authorization Strategy
+// Matrix Authorization Strategy
 def strategy = new GlobalMatrixAuthorizationStrategy()
 
-// Grant full permissions to your admin user
-def sidAdmin = "urjit21838321"
-strategy.add(Jenkins.ADMINISTER, sidAdmin)
-strategy.add(hudson.model.Item.BUILD, sidAdmin)
-strategy.add(hudson.model.Item.READ, sidAdmin)
-strategy.add(hudson.model.Run.DELETE, sidAdmin)
-strategy.add(hudson.model.Computer.BUILD, sidAdmin)
+// Full rights for admin
+strategy.add(Jenkins.ADMINISTER, "admin")
 
-// Grant limited permissions to pipeline-user
-def sidPipeline = "pipeline-user"
-strategy.add(hudson.model.Item.READ, sidPipeline)
-strategy.add(hudson.model.Item.BUILD, sidPipeline)
-strategy.add(hudson.model.Run.UPDATE, sidPipeline)
+// Limited rights for pipelineuser
+strategy.add(hudson.model.Item.READ, "pipelineuser")
+strategy.add(hudson.model.Item.BUILD, "pipelineuser")
 
-// Disable anonymous access (no permissions granted)
+// Disable anonymous access
 instance.setAuthorizationStrategy(strategy)
 instance.save()
 
-println("Security init applied: permissions set for urjit21838321 and pipeline-user, anonymous disabled.")
+println("Security init applied: admin and pipelineuser created.")
